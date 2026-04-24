ad1a8b94705314d29858460ea5243f3552938670
ruos5 <ows5@scarletmail.rutgers.edu>
2026-04-23 23:01:41 -0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const path = require("path"); //
+const path = require("path");
 const app = express();
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../frontend"))); //
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 // Placeholder signup route
 app.post("/signup", (req, res) => {
